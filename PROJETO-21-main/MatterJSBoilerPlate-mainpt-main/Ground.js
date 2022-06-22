4e9a9669324b1e444d51a52ec06520230088f0fb
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-06-22 17:42:21 -0300

    Add files via upload

--- PROJETO-21-main/MatterJSBoilerPlate-mainpt-main/Ground.js
+++ PROJETO-21-main/MatterJSBoilerPlate-mainpt-main/Ground.js
@@ -1,31 +1,31 @@
 class Ground
 {
-    constructor(x,y,h,w)
-    {
-        var options={
-            isStatic:true
-        };
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
 
-        
-        this.w = w;
-        this.h = h;
-        // está faltando o this.x e o this.y
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
 
-        this.body=Bodies.rectangle(x, y, w, h, options);
-        World.add(world,this.body)
-    }
-
-    show() {
-        var pos = this.body.position;
-        push();
-        // aqui tem um translate para te ermitir mudar a posição x e y
-        // translate(pos.x,pos.y)
-        rectMode(CENTER);
-        stroke(255);
-        fill(127);
-        //como vc usou o translate aqui no ect o x e o y tem quue ser 0
-        rect(pos.x,pos.y.this.w,this.h);
-        pop();
-    }
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			fill("darkblue")
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
 
 }
--- PROJETO-21-main/MatterJSBoilerPlate-mainpt-main/sketch.js
+++ PROJETO-21-main/MatterJSBoilerPlate-mainpt-main/sketch.js
@@ -1,22 +1,24 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var quadrado, bola, retanglo
-var engine, world
-var ball
+
+var world,engine;
+var ball;
 var ground;
-var left;
-var rigth;
-// falta declarar uma variavel para o raio colissor pode chamar de radius=20
+var left
+var right;
+ // falta de uma variavel para o raio colissor pode chamar de radius=40
+
+var radius = 40;
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
 	engine = Engine.create();
 	world = engine.world;
-
 
 	var options={
 		isStatic:false,
@@ -25,49 +27,44 @@ function setup() {
 		density:1.2
 	}
 
-	ball=Bodies.circle(260,100,radius/2,options)
-	World.add(world,ball)
+	ball = Bodies.circle(260,100,radius/2,options);
+	World.add(world,ball);
 
+	
 	// vc criou a classe mas não instanciou para dentro do codigo 
 	// vou fazer a primeira e vc faz as outras duas com base nela
 
-	ground = new Ground(width/2,670,width,20);
-	// left vau receber 1100,600,20,120
+	ground=new Ground(width/2,670,width,20);
+ 	// left vau receber 1100,600,20,120
+	left = new Ground(1100,600,20,120);
+	// right vai receber  1350,600,20,120
+	right = new Ground(1350,600,20,120);
 
-	// lright vai receber  1350,600,20,120
-
-
-
-
-	
-  	
+	Engine.run(engine);
+  
 }
 
 
 function draw() {
   rectMode(CENTER);
-  background("green");
+  background(0);
 
+  fill("skyblue")
+  ellipse(ball.position.x,ball.position.y,radius,radius);
 
-  ellipse(ball.position.x,ball.position.y,radius, radius)
-
-  ground.show();
-  left.show();
-  rigth.show();
-
-  Engine.run(engine);
- 
- 
-
-  drawSprites();
- 
+  ground.display();
+  left.display();  
+  right.display();
+  
 }
 
 // vc vai criar uma função keyPressed
+function keyPressed(){
 
-// e vai criar um if para verificar se o codigo da tecla de UP_ARROW
-
-//foi pracionada e se foi
-//Matter.Body.applyForce(ball,ball.position,{x:85,y:-85})
+}
 
 
+// // e vai criar um if para verificar se o codigo da tecla de UP_ARROW
+
+// //foi pracionada e se foi
+// //Matter.Body.applyForce(ball,ball.position,{x:85,y:-85})
